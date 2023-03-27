45dd6bd09892d1703cc165d2f3b3d1a67680b6f0
RicardoRG73 <ricardo.roman@umich.mx>
2023-03-27 13:43:50 -0600

    ex01 geometry points created

--- ex01.py
+++ ex01.py
@@ -11,25 +11,22 @@ import calfem.vis_mpl as cfv
 g = cfg.Geometry()          # geometry object
 
 # points
-g.point([0, 0])    # 0
-g.point([0.5, 0], el_size=0.5)    # 1
-g.point([2, 0])    # 2
-g.point([2, 1])    # 3
-g.point([1.5, 1], el_size=0.5)    # 4
-g.point([0, 1])    # 5
+delta_i = 1e-1
+g.point([-1,0])
+g.point([-0.5-delta_i,0])
+g.point([-0.5+delta_i,0])
+g.point([1,0])
+g.point([1,1])
+g.point([0.5+delta_i,1])
+g.point([0.5-delta_i,1])
+g.point([-1,1])
 
 # lines
 left = 11       # marker for nodes on left boundary
 right = 12      # marker for nodes on right boundary
 neumann = 13    # marker for nodes on neumman boundaries (up, down)
 interface = 14  # marker for nodes on interface
-g.spline([0,1], marker=neumann)    # 0
-g.spline([1,2], marker=neumann)    # 1
-g.spline([2,3], marker=right)      # 2
-g.spline([3,4], marker=neumann)    # 3
-g.spline([4,5], marker=neumann)    # 4
-g.spline([5,0], marker=left)       # 5
-g.spline([1,4], marker=interface)  # 6
+
 
 # surfaces
 mat0 = 100      # marker for nodes on material 1
