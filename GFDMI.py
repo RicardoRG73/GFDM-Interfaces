f12ad5c2bf83fab11142d3a2bc2fcf1f8c34fbd5
RicardoRG73 <ricardo.roman@umich.mx>
2023-08-17 12:34:52 -0600

    No double nodes - code lines comented

--- GFDMI.py
+++ GFDMI.py
@@ -150,25 +150,26 @@ def create_system_K_F(
         m = b.shape[0]                              # m: number of nodes at interfaace
         N = p.shape[0]                              # N: number of total nodes in the domain
 
-        # Double nodes
-        p = np.vstack((p, p[b,:]))                  # double nodes coordinated added to original coordinates
-        #   K extension
-        K = sp.hstack((K, sp.lil_matrix((N,m))))
-        K = sp.vstack((K, sp.lil_matrix((m,N+m))))
-        K = sp.lil_matrix(K)
+        # # Double nodes
+        # p = np.vstack((p, p[b,:]))                  # double nodes coordinated added to original coordinates
+        # #   K extension
+        # K = sp.hstack((K, sp.lil_matrix((N,m))))
+        # K = sp.vstack((K, sp.lil_matrix((m,N+m))))
+        # K = sp.lil_matrix(K)
 
-        bd = np.arange(N,N+m)                       # index for double nodes
+        # bd = np.arange(N,N+m)                       # index for double nodes
 
-        K[bd,bd] = 1
-        K[bd,b] = -1
+        # K[bd,bd] = 1
+        # K[bd,b] = -1
 
-        #   F extension
-        F = sp.vstack((F, sp.lil_matrix((m,1))))
-        F = sp.lil_matrix(F)
-        for i in bd:
-            F[i] = alpha(p[i])
+        # #   F extension
+        # F = sp.vstack((F, sp.lil_matrix((m,1))))
+        # F = sp.lil_matrix(F)
+        # for i in bd:
+        #     F[i] = alpha(p[i])
 
-        i2 = N
+        # i2 = N
+        
         for i in b:
             # Material M0, whit b interface-original-nodes
             I_all = support_nodes(i,triangles)
@@ -248,19 +249,19 @@ def create_system_K_F(
             Gamma_n = Gamma_n[1:]
             Gg = Gamma_ghost / Gamma_n_ghost
 
-            # center double node conected to double nodes, instead of original nodes
-            Ib = np.setdiff1d(I1,m1)                         # index for nodes at interface
-            Ib = np.setdiff1d(Ib,I1[0])                      # leaving out the central node
-            for j in Ib:
-                dist = np.sum((p[j] - p[bd])**2, axis=1)    # distances for interface node j to original interface
-                I1[I1==j] = bd[dist==0]                     # modified index for interface node j
+            # # center double node conected to double nodes, instead of original nodes
+            # Ib = np.setdiff1d(I1,m1)                         # index for nodes at interface
+            # Ib = np.setdiff1d(Ib,I1[0])                      # leaving out the central node
+            # for j in Ib:
+            #     dist = np.sum((p[j] - p[bd])**2, axis=1)    # distances for interface node j to original interface
+            #     I1[I1==j] = bd[dist==0]                     # modified index for interface node j
                 
-            I1[0] = i2                                      # center node index modified to double node
+            # I1[0] = i2                                      # center node index modified to double node
             
             beta_i = beta(p[i])
             K[i,I1] += Gamma - Gg * Gamma_n
             F[i] = F[i].toarray() - Gg * beta_i/2
-            i2 += 1
+            # i2 += 1
                 
 
     # Dirichlet boundaries
