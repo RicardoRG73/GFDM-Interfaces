e816a3c30277a5eb6ad4fb67ba90ceddcd91becf
RicardoRG73 <ricardo.roman@umich.mx>
2024-02-21 16:13:08 -0600

    create_system_k_f modified dirichlet rows for stability

--- GFDMI.py
+++ GFDMI.py
@@ -486,9 +486,9 @@ def create_system_K_F_cont_U(
         b = dirichlet_boundaries[boundary][0]
         u = dirichlet_boundaries[boundary][1]
         for i in b:
-            F -= K[:,i] * u(p[i])
+            # F -= K[:,i] * u(p[i])
             F[i] = u(p[i])
-            K[:,i] = 0
+            # K[:,i] = 0
             K[i,i] = 1
 
     K = sp.csr_matrix(K)
